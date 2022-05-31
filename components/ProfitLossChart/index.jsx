16b2955812d229be0d896b3883c2b2d519214a1d
Ben Andrew <benandrew89@gmail.com>
2022-05-31 14:55:47 +0100

    Refactor LineChart

--- components/ProfitLossChart/index.jsx
+++ components/ProfitLossChart/index.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import * as Recharts from "recharts";
-import "./ProfitLossChart.css";
+import LineChart from "../charts/LineChart";
 
 const balanceUrl = `${process.env.API_URL}profit-losses?sort=Month`;
 
@@ -24,40 +23,7 @@ function ProfitLossChart() {
       )
   }, []);
 
-  const args = {
-    width: 370,
-    height: 150,
-    margin: { top: 0, right: 0, left: 0, bottom: 0 },
-    line1Color: "#00A1E4",
-    line2Color: "#FF6250",
-    line1Key: "profit",
-    line2Key: "loss",
-    data: data,
-    align: "center",
-    verticalAlign: "bottom",
-    ShowCartesianGrid: true,
-    ShowXAxis: false,
-    ShowYAxis: false,
-    ShowTooltip: true,
-    ShowLegend: false,
-    xAxisDataKey: "",
-    widgetCategory: "6216125a39298487a332afed",
-    widgetCategoryName: "Charts",
-  };
-
-  return (
-    <div className="simple-line-chart">
-      <Recharts.LineChart {...args}>
-        {args.ShowCartesianGrid && <Recharts.CartesianGrid strokeDasharray="3 3" />}
-        <Recharts.XAxis dataKey="name" hide={!args.ShowXAxis} />
-        <Recharts.YAxis hide={!args.ShowYAxis} />
-        {args.ShowTooltip && <Recharts.Tooltip />}
-        {args.ShowLegend && <Recharts.Legend align={args.align} verticalAlign={args.verticalAlign} />}
-        <Recharts.Line type="monotone" dataKey={args.line1Key} stroke={args.line1Color} dot={false} />
-        <Recharts.Line type="monotone" dataKey={args.line2Key} stroke={args.line2Color} dot={false} />
-      </Recharts.LineChart>
-    </div>
-  );
+  return <LineChart data={data} lines={[{key: "profit", color: "#00A1E4"}, {key: "loss", color: "#FF6250"}]} />;
 }
 
 export default ProfitLossChart;
---
+++ components/charts/LineChart/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import * as Recharts from "recharts";
+import "./LineChart.css";
+
+function LineChart({ data, lines }) {
+  const args = {
+    width: 370,
+    height: 150,
+    margin: { top: 0, right: 0, left: 0, bottom: 0 },
+    data: data,
+    align: "center",
+    verticalAlign: "bottom",
+    ShowCartesianGrid: true,
+    ShowXAxis: false,
+    ShowYAxis: false,
+    ShowTooltip: true,
+    ShowLegend: false,
+    xAxisDataKey: "",
+    widgetCategory: "6216125a39298487a332afed",
+    widgetCategoryName: "Charts",
+  };
+  
+  return (
+    <div className="simple-line-chart">
+      <Recharts.LineChart {...args}>
+        {args.ShowCartesianGrid && <Recharts.CartesianGrid strokeDasharray="3 3" />}
+        <Recharts.XAxis dataKey="name" hide={!args.ShowXAxis} />
+        <Recharts.YAxis hide={!args.ShowYAxis} />
+        {args.ShowTooltip && <Recharts.Tooltip />}
+        {args.ShowLegend && <Recharts.Legend align={args.align} verticalAlign={args.verticalAlign} />}
+        {lines.map((line) => (<Recharts.Line type="monotone" key={line.key} dataKey={line.key} stroke={line.color} dot={false} />))}
+      </Recharts.LineChart>
+    </div>
+  );
+}
+
+export default LineChart;
