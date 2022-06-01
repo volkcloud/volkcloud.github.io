ce088e0945d1717dd765b30062e2f566f0238ed0
Ben Andrew <benandrew89@gmail.com>
2022-06-01 10:47:14 +0100

    Refactor side menu

--- components/Menu/index.jsx
+++ components/Menu/index.jsx
@@ -1,11 +1,44 @@
 import React from "react";
-import Menu2 from "../Menu2";
+import "antd/dist/antd.css";
+import "antd/dist/antd.variable.css";
+import * as AntDesignIcons from "@ant-design/icons";
+import * as Antd from "antd";
 import "./Menu.css";
 
 function Menu() {
   return (
     <div className="menu">
-      <Menu2 />
+      <div className="menu-1">
+        <Antd.Menu
+          style={{
+            width: 200,
+          }}
+          theme="light"
+        >
+          <Antd.Menu.Item key="508" icon={<AntDesignIcons.BarChartOutlined />}>
+            Dashboard
+          </Antd.Menu.Item>
+
+          <Antd.Menu.Item key="767" icon={<AntDesignIcons.UserOutlined />}>
+            Account
+          </Antd.Menu.Item>
+
+          <Antd.Menu.Item key="422" icon={<AntDesignIcons.SettingOutlined />}>
+            Settings
+          </Antd.Menu.Item>
+
+          <Antd.Menu.ItemGroup key="g543" title="Support">
+            <Antd.Menu.Item key="873" icon={<AntDesignIcons.WechatFilled />}>
+              Chat
+            </Antd.Menu.Item>
+
+            <Antd.Menu.Item key="179" icon={<AntDesignIcons.QuestionCircleFilled />}>
+              FAQ
+            </Antd.Menu.Item>
+            <Antd.Menu.Divider />
+          </Antd.Menu.ItemGroup>
+        </Antd.Menu>
+      </div>
     </div>
   );
 }
