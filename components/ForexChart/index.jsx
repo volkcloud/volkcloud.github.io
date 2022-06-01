e3f77ad2e662a0438948db637ceb6e7ce108f9ee
Ben Andrew <benandrew89@gmail.com>
2022-06-01 09:47:58 +0100

    Populate forex chart

--- components/ForexChart/index.jsx
+++ components/ForexChart/index.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FillChart from "../charts/FillChart";
 
+const forexUrl = `${process.env.API_URL}forex-balance-histories?sort=Month`;
+
 function ForexChart() {
-  const data = [
-    { name: "Jan", amount: 60 },
-    { name: "Feb", amount: 20 },
-    { name: "Mar", amount: 400 },
-    { name: "Apr", amount: 208 },
-    { name: "May", amount: 450 },
-    { name: "Jun", amount: 550 },
-  ];
+  const [data, setData] = useState([{name:"January", amount: 0}]);
+
+  useEffect(() => {
+    fetch(forexUrl, {headers: {Authorization: `Bearer ${process.env.API_TOKEN}`}})
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setData(result.data.map((item) => ({
+            name: new Date(item.attributes.Month).toLocaleString('default', { month: 'long' }),
+            amount: item.attributes.Amount,
+          })));
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+  }, []);
 
   return <FillChart data={data} dataKey="amount" height={80} tooltip={false} />;
 }
